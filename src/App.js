fe4727781b8359d64d8441f2b9a7b09e948fdd4b
magb7 <margaux.bedu@gmail.com>
2020-03-31 16:34:24 +0200

    quest 3 react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/53.jpg"
-        name="Vicki Washington"
-        online
-      />
-      <Contact 
-        avatar="https://randomuser.me/api/portraits/men/89.jpg"
-        name="Darryl Rodriquez"
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/59.jpg"
-        name="Pauline Vasquez"
-        online
-      />
+      <ContactList />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,15 +3,15 @@ import './Contact.css';
 import PropTypes from 'prop-types';
 
 
-const Contact = (props) => {
+const Contact = ({avatar, name, online}) => {
     return (
         <div className="Contact">
-            <img src={props.avatar} className="avatar" alt={props.name} />
+            <img src={avatar} className="avatar" alt={name} />
             <div>
-                <h4 className="name">{props.name}</h4>
+                <h4 className="name">{name}</h4>
                 <div className="status">
-                    <span className={props.online ? "status-online" : "status-offline"}></span>
-                    <p className="status-text" >{props.online ? "online" : "offline"}</p>
+                    <span className={online ? "status-online" : "status-offline"}></span>
+                    <p className="status-text" >{online ? "online" : "offline"}</p>
                 </div>
             </div>
         </div>
