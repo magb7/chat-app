704e5f0ec3045e0c635e350707f6da6b87149471
magb7 <margaux.bedu@gmail.com>
2020-04-06 11:35:44 +0200

    quest react 5 state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,19 +3,34 @@ import './Contact.css';
 import PropTypes from 'prop-types';
 
 
-const Contact = ({avatar, name, online}) => {
-    return (
-        <div className="Contact">
-            <img src={avatar} className="avatar" alt={name} />
-            <div>
-                <h4 className="name">{name}</h4>
-                <div className="status">
-                    <span className={online ? "status-online" : "status-offline"}></span>
-                    <p className="status-text" >{online ? "online" : "offline"}</p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: this.props.online,
+        }
+    }
+
+    render() {
+        return (
+            <div className="Contact">
+                <img src={this.props.avatar} className="avatar" alt={this.props.name} />
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div className="status"
+                        onClick={event => {
+                            const newStatus = !this.state.online;
+                            this.setState({
+                                online: newStatus
+                            });
+                        }}>
+                        <span className={this.state.online ? "status-online" : "status-offline"}></span>
+                        <p className="status-text" >{this.state.online ? "online" : "offline"}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 Contact.propTypes = {
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -44,3 +44,4 @@ const ContactList = () => {
 };
 
 export default ContactList;
+
